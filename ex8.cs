2ffc56a1f8b93530baeda3abc41f0670f29809d3
Felpps56 <torresfelippe53@gmail.com>
2025-02-18 16:40:48 -0300

    Update ex8.cs

--- ex8.cs
+++ ex8.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 
 namespace Lista_2_Atividades
@@ -8,25 +8,29 @@ namespace Lista_2_Atividades
 	{
 		public static void Main()
 		{
-		/*8. Escreva um programa em C# para encontrar a soma dos 10 primeiros
-			números naturais.*/
+			/*8. Escreva um programa em C# para encontrar a soma dos 10 primeiros
+				números naturais.*/			
 			
-		int soma = 0;
+			
+			int soma = 0;
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++)
+		{
 			Console.WriteLine(i);
 			soma += 1;
 		}
 		Console.WriteLine("A soma dos números naturais é {0}:", + soma);
-			
-			
-			
-			
-			
-			
-			
-			
-			Console.ReadKey(true);
+		
+		int j = 0;
+		while(j < 10)
+		{
+		Console.Write("{0} ", j);
+		soma += j;
+		j++;
+		}
+					
+		Console.ReadKey(true);	
 		}
 	}
 }
+
